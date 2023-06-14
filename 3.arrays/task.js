147b16f6df522881544d18c9c47e8567804e1d61
Engel62 <sr.engel@bk.ru>
2023-06-14 14:00:21 +0300

    задание готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,9 @@
 function compareArrays(arr1, arr2) {
-  
+    let result = arr1.length===arr2.length && arr1.every((element, n) => element===arr2[n]);
+    return result;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let result1 = users.filter(el => el.gender === gender).reduce((accum, averageAge, index, arrayLength) => accum + averageAge.age / arrayLength.length, 0);
+    return result1
 }
