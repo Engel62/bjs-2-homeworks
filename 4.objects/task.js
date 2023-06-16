4b723af3a2b16ce2876fa24aa194b459bdc854a5
Engel62 <sr.engel@bk.ru>
2023-06-16 13:19:48 +0300

    задание готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,41 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
+
+
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
+
+
+Student.prototype.addMark = function (mark) {
+    if(this.marks === undefined) {
+        this.marks = [mark];
+    } else {
+        this.marks.push(mark);
+    }
+}
+
 
 Student.prototype.addMarks = function (...marks) {
-  
+    return this.marks ? this.marks.push(...marks) : "stud expelled"
 }
+
 
 Student.prototype.getAverage = function () {
-  
+    return this.marks && this.marks.length ?
+        this.marks.reduce   (
+            (counter,value) => counter + value, 0
+        ) / this.marks.length : 0;
 }
 
+
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
